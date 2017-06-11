d1524e94e7fe9637b17563f3079fe1d9f2e6f6d2
H.T. Koo <h1017760@connect.hku.hk>
2017-06-11 23:48:49 +0800

    FreeCodeCamp - Back End Development Certification - API projects: updated index.js with usage and usage of timestampParser.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+
+var timestampParser = require("../service/timestampParser");
+
+var DATE_FORMAT = "MMMM d, YYYY";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {});
+});
+
+router.get('/:timestamp', function (req, res, next) {
+    var timestamp = req.params.timestamp;
+    res.send(timestampParser(timestamp));
 });
 
 module.exports = router;
