b73b438e1b1ede215eb7c31e16e0d17a482e2a13
H.T. Koo <h1017760@connect.hku.hk>
2017-06-12 09:04:44 +0800

    FreeCodeCamp - Back End Development Certification - API projects: added tests for natural language date and unix timestamp cases; added implementation

--- service/test-unit/timestampParser.Test.js
+++ service/test-unit/timestampParser.Test.js
@@ -8,9 +8,81 @@ var timestampParser = require('../timestampParser');
 
 describe("timestampParser", function () {
     [
+        {
+            "input": "0",
+            "expectedResponse": {
+                "unix": 0,
+                "natural": "January 1, 1970"
+            }
+        },
+        {
+            "input": "1",
+            "expectedResponse": {
+                "unix": 1,
+                "natural": "January 1, 1970"
+            }
+        },
+        {
+            "input": "86399",
+            "expectedResponse": {
+                "unix": 86399,
+                "natural": "January 1, 1970"
+            }
+        },
+        {
+            "input": "86400",
+            "expectedResponse": {
+                "unix": 86400,
+                "natural": "January 2, 1970"
+            }
+        }
+    ].forEach(function (params) {
+        it(format("should return {} for {} which is a unix timestamp", JSON.stringify(params.expectedResponse), params.input), function () {
+            //    given
+            //    when
+            var response = timestampParser(params.input);
+
+            //    then
+            test.expect(response).to.deep.equal(params.expectedResponse);
+        });
+    });
+    [
+        {
+            "input": "January 1, 2016",
+            "expectedResponse": {
+                "unix": 1451606400,
+                "natural": "January 1, 2016"
+            }
+        },
+        {
+            "input": "January 1, 1970",
+            "expectedResponse": {
+                "unix": 0,
+                "natural": "January 1, 1970"
+            }
+        },
+        {
+            "input": "January 2, 1970",
+            "expectedResponse": {
+                "unix": 86400,
+                "natural": "January 2, 1970"
+            }
+        }
+    ].forEach(function (params) {
+        it(format("should return {} for {} which is a natural language date", JSON.stringify(params.expectedResponse), params.input), function () {
+            //    given
+            //    when
+            var response = timestampParser(params.input);
+
+            //    then
+            test.expect(response).to.deep.equal(params.expectedResponse);
+        });
+    });
+
+    [
         "some broken string",
         "123a"
-    ].forEach(function(str){
+    ].forEach(function (str) {
         it(format("should return null for {} which is not unix timestamp nor natural language date", str), function () {
             //    given
             //    when
--- service/timestampParser.js
+++ service/timestampParser.js
@@ -1,7 +1,49 @@
 /**
  * Created by Hey on 11 Jun 2017
  */
+var moment = require('moment');
+var DATE_FORMAT = "MMMM D, YYYY";
+var NUM_OF_MILLISEC_IN_A_SECOND = 1000;
+
+// From https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+function filterInt(value) {
+    if (/^([-+])?([0-9]+|Infinity)$/.test(value))
+        return Number(value);
+    return NaN;
+}
+
+function isUnixTimeStamp(timestamp) {
+    return !isNaN(filterInt(timestamp));
+}
+
+function isNaturalLanguageDate(timestamp) {
+    return !isNaN(Date.parse(timestamp));
+}
+
+function toUnix(decodedTimestamp) {
+    return moment.utc(decodedTimestamp, DATE_FORMAT).unix();
+}
+
+function toNatural(timestampInInt) {
+    return moment.utc(moment.unix(timestampInInt)).format(DATE_FORMAT);
+}
+
+
+function createResponseJson(unix, natural) {
+    return {
+        "unix": unix,
+        "natural": natural
+    };
+}
 
 module.exports = function (timestamp) {
-    return "null";
+    if (isUnixTimeStamp(timestamp)) {
+        var timestampInInt = filterInt(timestamp);
+        return createResponseJson(timestampInInt, toNatural(timestampInInt));
+    } else if (isNaturalLanguageDate(timestamp)) {
+        var decodedTimestamp = decodeURI(timestamp);
+        return createResponseJson(toUnix(decodedTimestamp), decodedTimestamp);
+    } else {
+        return "null";
+    }
 };
