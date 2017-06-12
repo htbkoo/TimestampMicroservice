a9123e50068fc007b935dc8a5822fd4482f6c3ae
H.T. Koo <h1017760@connect.hku.hk>
2017-06-12 09:04:16 +0800

    FreeCodeCamp - Back End Development Certification - API projects: cleaned up index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
 
 var timestampParser = require("../service/timestampParser");
-
-var DATE_FORMAT = "MMMM d, YYYY";
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
